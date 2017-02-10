fe918444a2b389842b04d01269c0cd38204e9131
Suguru Tokuda <suguru.tokuda@gmail.com>
2017-02-10 16:22:15 -0600

    collision implemented

--- core/src/com/sugurugreg/game/states/PlayState.java
+++ core/src/com/sugurugreg/game/states/PlayState.java
@@ -1,37 +1,77 @@
 package com.sugurugreg.game.states;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.Array;
 import com.sugurugreg.game.SkiGame;
+import com.sugurugreg.game.sprites.Bird;
+import com.sugurugreg.game.sprites.Tube;
 
 /**
  * Created by Suguru on 2/9/17.
  */
 
 public class PlayState extends State {
-    private Texture bird;
+    private static final int TUBE_SPACING = 125;
+    private static final int TUBE_COUNT = 4;
+
+    private Bird bird;
+    private Texture bg;
+
+    private Array<Tube> tubes;
+
 
     protected PlayState(GameStateManager gsm) {
         super(gsm);
-        bird = new Texture("bird.png");
+        bird = new Bird(50, 300);
         cam.setToOrtho(false, SkiGame.WIDTH / 2, SkiGame.HEIGHT / 2);
+        bg = new Texture("background.png");
+
+        tubes = new Array<Tube>();
+
+        for (int i = 0; i < TUBE_COUNT; i++) {
+            tubes.add(new Tube(i * (TUBE_SPACING + Tube.TUBE_WIDTH)));
+        }
+
     }
 
     @Override
     protected void handleInput() {
-
+        if(Gdx.input.justTouched()) {
+            bird.jump();
+        }
     }
 
     @Override
     public void update(float dt) {
+        handleInput();
+        bird.update(dt);
+        cam.position.x = bird.getPosition().x + 80;
 
+        for (Tube tube : tubes) {
+            if (cam.position.x - (cam.viewportWidth / 2) > tube.getPosTopTube().x + tube.getTopTube().getWidth()) {
+                tube.reposition(tube.getPosTopTube().x + ((Tube.TUBE_WIDTH + TUBE_SPACING) * TUBE_COUNT));
+            }
+
+            if (tube.collides(bird.getBounds())) {
+                gsm.set(new PlayState(gsm));
+            }
+        }
+
+        cam.update();
     }
 
     @Override
     public void render(SpriteBatch sb) {
         sb.setProjectionMatrix(cam.combined);
         sb.begin();
-        sb.draw(bird, 50, 50);
+        sb.draw(bg, cam.position.x - (cam.viewportWidth / 2), 0);
+        sb.draw(bird.getTexture(), bird.getPosition().x, bird.getPosition().y);
+        for (Tube tube: tubes) {
+            sb.draw(tube.getTopTube(), tube.getPosTopTube().x, tube.getPosTopTube().y);
+            sb.draw(tube.getBottomTube(), tube.getPosBotTube().x, tube.getPosBotTube().y);
+        }
         sb.end();
     }
 
