1f0b191bbb43264eac4b2443d7ed9be6fe50a6fa
Suguru Tokuda <suguru.tokuda@gmail.com>
2017-02-09 21:43:21 -0600

    Initial commit

---
+++ android/src/com/sugurugreg/game/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.sugurugreg.game;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.sugurugreg.game.SkiGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new SkiGame(), config);
+	}
+}
